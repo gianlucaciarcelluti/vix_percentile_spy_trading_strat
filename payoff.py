234ccaeb4189f5293a77a20da1c933ebe4029b2d
Gianluca Ciarcelluti <gianluca.ciarcelluti@acube.ai>
2024-11-03 11:10:13 +0100

    merged payoff with bear call

--- payoff.py
+++ payoff.py
@@ -7,26 +7,33 @@ def payoff_short_put(strike_price, premium, stock_price, multiplier=5):
 def payoff_long_call(strike_price, premium, stock_price, multiplier=100):
     return np.where(stock_price > strike_price, multiplier * (stock_price - strike_price - premium), -multiplier * premium)
 
+def payoff_short_call(strike_price, premium, stock_price, multiplier=100):
+    return np.where(stock_price > strike_price, multiplier * (premium - (stock_price - strike_price)), multiplier * premium)
+
 def plot_combined_payoff_chart():
     mes_strike_price = 5800
     mes_premium = 20
     vix_long_call_strike_price = 28
     vix_long_call_premium = 0.6
+    vix_short_call_strike_price = 70
+    vix_short_call_premium = 0.1
     vix_multiplier = 2
     initial_vix_price = 20
     initial_capital = 3000  # Define your initial capital here
-    stock_prices = np.linspace(mes_strike_price * 0.8, mes_strike_price * 1.05, 100)
+    stock_prices = np.linspace(mes_strike_price * 0.7, mes_strike_price * 1.1, 100)
     
     mes_payoffs = payoff_short_put(mes_strike_price, mes_premium, stock_prices)
     vix_prices = initial_vix_price + (mes_strike_price - stock_prices) * (200 / mes_strike_price)  # Assuming VIX increases 20 points for each 10% MES decrease
     vix_long_call_payoffs = payoff_long_call(vix_long_call_strike_price, vix_long_call_premium, vix_prices, vix_multiplier * 100)
+    vix_short_call_payoffs = payoff_short_call(vix_short_call_strike_price, vix_short_call_premium, vix_prices, vix_multiplier * 100)
     
-    combined_payoffs = mes_payoffs + vix_long_call_payoffs
+    combined_vix_payoffs = vix_long_call_payoffs + vix_short_call_payoffs
+    combined_payoffs = mes_payoffs + combined_vix_payoffs
 
     plt.figure(figsize=(10, 6))
     plt.plot(stock_prices, mes_payoffs, label='Short Put on MES @ ' + str(mes_strike_price))
-    plt.plot(stock_prices, vix_long_call_payoffs, label='Long Calls on VIX @ ' + str(vix_long_call_strike_price))
-    plt.plot(stock_prices, combined_payoffs, label='Combined Payoff', color='green')
+    plt.plot(stock_prices, combined_vix_payoffs, label='Bear Call VIX @ ' + str(vix_long_call_strike_price) + "/" + str(vix_short_call_strike_price), color='orange')
+    plt.plot(stock_prices, combined_payoffs, label='Combined', color='green')
     plt.axhline(0, color='black', lw=2)
     
     max_loss = np.min(combined_payoffs)
